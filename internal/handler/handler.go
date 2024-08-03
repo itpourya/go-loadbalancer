706feec3567f84cfa3dea26b69db64f1fd41e220
Pourya Samimi <neverdiegenius@gmail.com>
2024-08-03 06:40:50 +0330

    Add: create http handler for forward request between servers

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,23 @@
 package handler
+
+import (
+	"github.com/itpourya/go-loadbalancer/internal/loadbalancer"
+	"github.com/itpourya/go-loadbalancer/internal/server"
+	"net/http"
+)
+
+var (
+	ServerList = []*server.Server{
+		server.NewServer("server-1", "http://127.0.0.1:8080"),
+		server.NewServer("server-2", "http://127.0.0.1:8081"),
+		server.NewServer("server-3", "http://127.0.0.1:8082"),
+		server.NewServer("server-4", "http://127.0.0.1:8083"),
+		server.NewServer("server-5", "http://127.0.0.1:8084"),
+	}
+)
+
+func ForwardRequest(res http.ResponseWriter, req *http.Request) {
+	srv := loadbalancer.GetHealthyServer(ServerList)
+
+	srv.ReverseProxy.ServeHTTP(res, req)
+}
