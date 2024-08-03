ebf53839430cfbc387dc2bcb6863a02e6d975cd0
Pourya Samimi <neverdiegenius@gmail.com>
2024-08-03 06:43:14 +0330

    Add: implement load balancer logic

--- internal/loadbalancer/loadBalancer.go
+++ internal/loadbalancer/loadBalancer.go
@@ -1,1 +1,29 @@
-package loadBalancer
+package loadbalancer
+
+import (
+	"github.com/itpourya/go-loadbalancer/internal/server"
+	"log/slog"
+)
+
+var (
+	lastServedIndex = 0
+)
+
+func getServer(serverListCount int) int {
+	nextIndex := (lastServedIndex + 1) % serverListCount
+	lastServedIndex = nextIndex
+
+	return nextIndex
+}
+
+func GetHealthyServer(serverList []*server.Server) *server.Server {
+	for i := 0; i < len(serverList); i++ {
+		mark := getServer(len(serverList))
+		if serverList[mark].Health {
+			return serverList[mark]
+		}
+	}
+
+	slog.Warn("No healthy server exist")
+	return nil
+}
