4a7996964a0785332d3a989443fbf5f667f99fef
Pourya Samimi <neverdiegenius@gmail.com>
2024-08-03 06:44:00 +0330

    Add: implement server struct for create new server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,28 @@
 package server
+
+import (
+	"log/slog"
+	"net/http/httputil"
+	url2 "net/url"
+)
+
+type Server struct {
+	Name         string
+	URL          string
+	ReverseProxy *httputil.ReverseProxy
+	Health       bool
+}
+
+func NewServer(name, url string) *Server {
+	urlParse, err := url2.Parse(url)
+	if err != nil {
+		slog.Error("Can not parse url")
+		return nil
+	}
+
+	return &Server{
+		Name:         name,
+		URL:          url,
+		ReverseProxy: httputil.NewSingleHostReverseProxy(urlParse),
+	}
+}
