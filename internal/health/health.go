b3125d51c1e2f316eef1e70a28ec44171dc25755
Pourya Samimi <neverdiegenius@gmail.com>
2024-08-03 06:42:05 +0330

    Add: create cron job for check server health every 2 second

--- internal/health/health.go
+++ internal/health/health.go
@@ -1,1 +1,52 @@
 package health
+
+import (
+	"fmt"
+	"github.com/go-co-op/gocron/v2"
+	"github.com/itpourya/go-loadbalancer/internal/server"
+	"log/slog"
+	"net/http"
+	"time"
+)
+
+func StartHealthCheck(server []*server.Server) {
+	cr, _ := gocron.NewScheduler()
+
+	_, err := cr.NewJob(gocron.DurationJob(2*time.Second), gocron.NewTask(func() {
+		for _, host := range server {
+			healthy := checkHealth(host)
+
+			if healthy {
+				slog.Info(host.Name + " is healthy")
+			} else {
+				slog.Info(host.Name + " is not healthy")
+			}
+		}
+	}))
+	if err != nil {
+		fmt.Println(err)
+		slog.Error(err.Error())
+	}
+
+	cr.Start()
+
+	select {}
+
+	cr.Shutdown()
+}
+
+func checkHealth(server *server.Server) bool {
+	response, err := http.Head(server.URL)
+	if err != nil {
+		server.Health = false
+		return server.Health
+	}
+
+	if response.StatusCode != 200 {
+		server.Health = false
+		return server.Health
+	}
+
+	server.Health = true
+	return server.Health
+}
